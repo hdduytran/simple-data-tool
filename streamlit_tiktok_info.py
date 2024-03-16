2253948fae8cb9e98bd1edc8879bedcd8e703d21
Duy Tran <ducduyleo@gmai.com>
2024-03-16 17:12:35 +0700

    first commit

---
+++ streamlit_tiktok_info.py
@@ -0,0 +1,125 @@
+import requests
+import re
+from common import *
+import pandas as pd
+from utils import *
+from Parallelizer import make_parallel
+import streamlit as st
+import streamlit as st
+import streamlit_authenticator as stauth
+from yaml.loader import SafeLoader
+import yaml
+import os
+
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+print(config)
+config['credentials']['usernames']["admin"]["password"] = os.getenv("ADMIN-PASSWORD")
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+
+st.title("Tiktok Info")
+
+
+def get_info(username):
+    url = f"https://tokapi-mobile-version.p.rapidapi.com/v1/user/@{username}"
+
+    headers = {
+        "X-RapidAPI-Key": os.getenv("RAPIDAPI_KEY"),
+        "X-RapidAPI-Host": "tokapi-mobile-version.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()
+
+def get_posts(user_id):
+    url = f"https://tokapi-mobile-version.p.rapidapi.com/v1/post/user/{user_id}/posts"
+
+    querystring = {"offset":"0","count":"10","region":"VN","with_pinned_posts":"1"}
+
+    headers = {
+        "X-RapidAPI-Key": "d1583bfe26msh53d8ca2e32d95c3p182a27jsn990cb23d366c",
+        "X-RapidAPI-Host": "tokapi-mobile-version.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    return response.json()
+
+def url2username(url):
+    return re.search(r"tiktok.com/@(\w+)", url).group(1)
+
+MAPPING_POST = {
+    "share_url": ["share_url"],
+    "create_time": ["create_time"],
+}
+
+
+@make_parallel
+def post_transform(post):
+    return Common.mapping_data(post, MAPPING_POST)
+
+
+MAPPING = {
+    "username": ["user","unique_id"],
+    "follower": ["user","follower_count"],
+    "bio_url": ["user","bio_url"],
+    "bio": ["user","signature"],
+    "sec_uid": ["user","sec_uid"],
+    "has_shop": ["user","tab_settings","shop_tab","show_shop_tab"],
+    "uid": ["user","uid"],
+}
+
+
+authenticator.login()
+if st.session_state["authentication_status"]:
+
+    links = st.text_area("Enter Tiktok links", "https://www.tiktok.com/@tiktok")
+    if links and st.button("Get Info"):
+        with st.spinner("Getting info..."):
+            info_placeholder = st.empty()
+            df_placeholder = st.empty()
+            links = links.split("\n")
+            results = []
+            for link in links:
+                username = url2username(link)
+                info_placeholder.markdown(f"Getting info for **{username}**...")
+                try:
+                    info = get_info(username)
+                    transformed = Common.mapping_data(info, MAPPING)
+                    post_list = get_posts(transformed["uid"])
+                    try:
+                        post_list = post_list["aweme_list"]
+                        post_list_transformed = post_transform(post_list)
+                        latest_post = max(post_list_transformed, key=lambda x: x["create_time"])
+                        if latest_post:
+                            minimal_url = latest_post["share_url"].split("?")[0]
+                            transformed["latest_post"] = minimal_url
+                        results.append(transformed)
+                    except Exception as e:
+                        print(f"Error getting posts for {username}: {e}")
+                        results.append(transformed | {"error": str(e)})
+                except Exception as e:
+                    print(f"Error getting info for {username}: {e}")
+                    results.append({"username": username, "error": str(e)})
+                df = pd.DataFrame(results)
+                df["follower"] = df["follower"].apply(lambda x: big_number_to_string_number(x))
+                df["other_url"] = df["bio"].apply(lambda x: extract_url(x) if x else None)
+                df["url"] = df["username"].apply(lambda x: f"https://www.tiktok.com/@{x}")
+                df["bio"] = df["bio"] + df["bio_url"]
+                cols = ["url","sec_uid","latest_post","follower","has_shop","bio"]
+                df = df[cols]
+                # df.to_csv("tiktok.csv", index=False)
+                df_placeholder.write(df)
+      
+elif st.session_state["authentication_status"] is False:
+    st.error('username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
---
+++ utils.py
@@ -0,0 +1,86 @@
+
+import re
+from urlextract import URLExtract
+
+
+
+def extract_url(text):
+    try:
+        extractor = URLExtract()
+        urls = extractor.find_urls(text)
+        return list(set(urls))
+    except:
+        return []
+def convert_big_number(x):
+    if x is None:
+        return x
+    x= x.replace(",", ".")
+    if x is None:
+        return x
+    if "k" in x:
+        return float(x.replace("k", "")) * 1000
+    if "m" in x:
+        return float(x.replace("m", "")) * 1000000
+    return float(x)
+
+def parse_price(price):
+    if price is None:
+        return None, None
+    if "-" not in price:
+        min_price =None
+        max_price = price.replace("₫", "").replace(".", "").strip()
+    else:
+        min_price, max_price = price.split(" - ")
+    if min_price is not None:
+        min_price = min_price.replace("₫", "").replace(".", "").strip()
+    if max_price is not None:
+        max_price = max_price.replace("₫", "").replace(".", "").strip()
+    return min_price, max_price
+
+def convert_price(price):
+    if price and type(price) == str:
+        price_characters = ["K", "M", "B"]
+        for i, character in enumerate(price_characters):
+            if character in price or character.lower() in price:
+                price = price.replace(character, "0" * (3 - i))
+                price = price.replace(character.lower(), "0" * (3 - i))
+                break
+        if "," in price:
+            price = price.replace(",", "")
+            price = price[:-1]
+    return price
+
+
+def parse_input(path_string):
+    output = []
+    for i in path_string.split('.'):
+        if '[' in i:
+            output += map(lambda x: int(x) if x.isdigit() else x, i.replace(']', '').split('['))
+        else:
+            output.append(i)
+    return output
+
+def transform_link(input, shop_id, product_id):
+    # input: str = "Áo Thun Ngắn Tay Nam ATINO chất liệu 100% Cotton thoáng mát form Regular - Áo Phông Cộc Tay AP2.2103"
+    # output: str = "Áo-Thun-Ngắn-Tay-Nam-ATINO-chất-liệu-100-Cotton-thoáng-mát-form-Regular-Áo-Phông-Cộc-Tay-AP2.2103-i.439115986.12098818105"
+
+    # remove special characters
+    input = re.sub(r'[^\w\s\.]', '', input)
+    # remove multiple spaces
+    input = re.sub(r'\s+', '-', input)
+    # remove last - if exist
+    input = re.sub(r'-+$', '', input)
+
+    return f"https://shopee.vn/{input}-i.{shop_id}.{product_id}"
+
+    
+def big_number_to_string_number(x, round_to=1):
+    if not isinstance(x, (int, float)):
+        return x
+    if x is None:
+        return x
+    if x < 10000:
+        return str(x)
+    if x < 1000000:
+        return str(round(x / 1000, round_to)) + "K"
+    return str(round(x / 1000000, round_to)) + "M"
